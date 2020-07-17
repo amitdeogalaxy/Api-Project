ef81bc1a55c096c76942440288e44104a920c93c
Deo <adeo@Galaxe.com>
2020-07-17 12:21:05 +0530

    update 17/07

--- runtests.cmd
+++ runtests.cmd
@@ -6,7 +6,7 @@
     echo "MSBuild is not in your PATH. Please use a developer command prompt!"
     goto :end
 ) ELSE (
-    MSBuild.exe "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin"
+    MSBuild.exe "Active-Active.csproj"
 )
 
 @if ERRORLEVEL 1 goto end
